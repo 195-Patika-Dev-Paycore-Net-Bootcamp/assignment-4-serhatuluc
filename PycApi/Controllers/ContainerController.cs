56a8bf7e437332c181dc8f836d6e56063596de1a
serhatuluc <99399280+serhatuluc@users.noreply.github.com>
2022-09-02 13:38:13 -0700

    Pyc Api is uploaded

---
+++ PycApi/Controllers/ContainerController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PycApi.Base;
+using PycApi.Data.Model;
+using PycApi.Service;
+using System.Collections.Generic;
+
+namespace PycApi.Controllers
+{
+    [ApiController]
+    [Route("api/nhb/[controller]")]
+
+    public class ContainerController : ControllerBase
+    {
+        private readonly IContainerService containerService;
+   
+
+        public ContainerController(IVehicleService vehicleService, IContainerService containerService,IMapper mapper)
+        {
+            this.containerService = containerService;
+
+        }
+    
+
+        [HttpGet("GetClustered")]
+        public BaseResponse<List<List<Container>>> GetClustered(int id, int numCluster)
+        {
+            
+            return containerService.Clusterized(id,numCluster);
+            
+        }
+    }
+}
